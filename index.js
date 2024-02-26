b865d4f64d3a90c22222450add7515c6e9271ed6
FacuTello <facundostello@hotmail.com>
2024-02-26 13:47:57 -0300

    show city name done

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ function searchCityWeather(){
     let peticion = fetch("https://api.weatherapi.com/v1/forecast.json?key=534a48d967bf4420a33190751241701&q=" + input + "&lang=es&days=5");
 peticion
     .then(res => res.json())
-    .then(res => console.log(res.current.condition.text))
+    .then(res => {
+           let cityName = res.location.name;
+           let divCity = document.getElementById('city');
+           divCity.textContent = cityName;
+    });
+
+    //.then(res => console.log(res.current.condition.text))
 }
 
 button.addEventListener('click', searchCityWeather);
