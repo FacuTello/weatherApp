ee52fc6e088f0c78aeae78b66fa608cb77f3bf0f
FacuTello <facundostello@hotmail.com>
2024-02-26 14:03:35 -0300

    Display name city done

--- index.js
+++ index.js
@@ -9,27 +9,18 @@ function searchCityWeather(){
 peticion
     .then(res => res.json())
     .then(res => {
-           let cityName = res.location.name;
-           let divCity = document.getElementById('city');
-           divCity.textContent = cityName;
+        console.log(res);
+          showInformationInDOM(res);
     });
-
-    //.then(res => console.log(res.current.condition.text))
 }
 
+function showInformationInDOM(res){
+    let cityName = res.location.name;
+           let divCity = document.getElementById('city');
+           divCity.textContent = cityName;
+}
 button.addEventListener('click', searchCityWeather);
-/*let peticion = fetch("https://api.weatherapi.com/v1/forecast.json?key=534a48d967bf4420a33190751241701&q=buenos-aires&lang=es&days=6")
-peticion
-    .then(res => res.json())
-    .then(res => console.log(res))
- /*   .then(res => {
-        // Accede a la propiedad 'condition' y luego a 'text'
-        const conditionText = res.current.condition.text;
 
-        // Hacer algo con conditionText, como imprimirlo en la consola
-        console.log("Texto de la condición:", conditionText);
-    })
-*/
 
 
 
